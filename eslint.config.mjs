08d925cea77d17d91d44ec3283f9c2ee035a23d2
potakaaa <helbirog@gmail.com>
2025-02-08 23:42:23 +0800

    disabled eslint rules for production

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -18,6 +18,7 @@ const eslintConfig = [
       '@typescript-eslint/no-unused-expressions': 'off',
       '@typescript-eslint/no-explicit-any': 'off',
       '@typescript-eslint/no-unused-vars': 'off',
+      'react-hooks/exhaustive-deps': 'off',
     },
   }),
 ]
